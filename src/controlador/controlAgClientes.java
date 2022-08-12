dc0035243816ccb30eb587cbd3cdee6c9548c7d3
daca129 <105086706+daca129@users.noreply.github.com>
2022-08-12 13:04:43 -0600

    Ahora si AgClientes
    aaaaaaaaa

--- src/controlador/controlAgClientes.java
+++ src/controlador/controlAgClientes.java
@@ -4,10 +4,25 @@
  */
 package controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import modelo.consultas;
+import vista.AgregarCliente;
+import vista.VentanaClientes2;
+
 /**
  *
  * @author daniel
  */
-public class controlAgClientes {
+public class controlAgClientes implements ActionListener{
+    
+    AgregarCliente ventAgC =new AgregarCliente();
+    consultas modelo=new consultas();
+    VentanaClientes2 ventCli=new VentanaClientes2();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
