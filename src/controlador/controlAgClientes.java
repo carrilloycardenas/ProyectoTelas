9fc77b0827f5850a82c4b3a2d5100011e76466d7
daca129 <105086706+daca129@users.noreply.github.com>
2022-08-12 15:27:50 -0600

    avance

--- src/controlador/controlAgClientes.java
+++ src/controlador/controlAgClientes.java
@@ -40,7 +40,7 @@ public class controlAgClientes implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(ventAgC.btnAgregar==e.getSource()){
-            modelo.AgProducto(ventAg.txtNombreProd.getText(),ventAg.txtDescripcion.getText(),ventAg.txtMarca.getText(), ventAg.txtStock.getValue().toString(),ventAg.txtPrecio.getValue().toString(), ventAg.txtLocal.getSelectedItem().toString(), ventAg.txtColor.getText(), Integer.toString(ventAg.txtCategoria.getSelectedIndex()+1), Integer.toString(ventAg.txtCategoria2.getSelectedIndex()+1),Integer.toString(ventAg.txtCategoria4.getSelectedIndex()+1), Integer.toString(ventAg.txtCategoria3.getSelectedIndex()+1));
+            modelo.AgProducto(ventAgC.txtNomCli.getText(),ventAgC.txtApell.getText(),ventAgC.txtCalle.getText(), ventAgC.txtColonia.getText(),ventAgC.txtNumeroDire.getText(), ventAgC.txtCiudad.getText(), ventAgC.txtEstado.getText(), ventAgC.txtCP.getText(), ventAgC.txtTelNum.getText(),ventAgC.txtNomDue.getText(),ventAgC.txtParent.getText(),ventAgC.txtCorreo.getText());
         }
     }
     
