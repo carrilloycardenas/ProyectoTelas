a497bf481744a751c87851858df3b66023ed5cd9
IngCarrillo <105088732+IngCarrillo@users.noreply.github.com>
2022-08-15 19:35:43 -0600

    modEmpleados
    oi

--- src/controlador/controlModEmpleados.java
+++ src/controlador/controlModEmpleados.java
@@ -37,9 +37,40 @@ public class controlModEmpleados implements ActionListener {
         try{
             this.vista.txtNombre.setText(this.rsEmp.getString("Nombre"));
             this.vista.txtApellidos.setText(this.rsEmp.getString("Apellidos"));
-            this.vista.txtFecha.setText(this.rsEmp.getString("FechaN"));
+            this.vista.txtFecha.setText(this.rsEmp.getString("FechaNacimiento"));
+            this.vista.txtNSS.setText(this.rsEmp.getString("NumSS"));
+            this.vista.txtFechaCon.setText(this.rsEmp.getString("FechaContratacion"));
+            switch(this.rsEmp.getString("NombrePuesto")){
+                case "Gerente general":
+                    this.vista.cbPuesto.setSelectedIndex(0);
+                    break;
+                case "Supervisor":
+                    this.vista.cbPuesto.setSelectedIndex(1);
+                    break;
+                case "Gerente":
+                    this.vista.cbPuesto.setSelectedIndex(2);
+                    break;
+                case "Encargado de ventas":
+                    this.vista.cbPuesto.setSelectedIndex(3);
+                    break;
+                case "Cajero":
+                    this.vista.cbPuesto.setSelectedIndex(4);
+                    break;
+                case "Vendedor":
+                    this.vista.cbPuesto.setSelectedIndex(5);
+                    break;
+                case "Almacenista":
+                    this.vista.cbPuesto.setSelectedIndex(6);
+                    break;
+            }
+            this.vista.txtUsuario.setText(this.rsEmp.getString("Usuario"));
+            this.vista.txtContra.setText(this.rsEmp.getString("Contraseña"));
             this.vista.txtCalle.setText(this.rsEmp.getString("calle"));
             this.vista.txtColonia.setText(this.rsEmp.getString("Colonia"));
+            this.vista.txtNumC.setText(this.rsEmp.getString("numCasa"));
+            this.vista.txtCiudad.setText(this.rsEmp.getString("Ciudad"));
+            this.vista.txtEstado.setText(this.rsEmp.getString("Estado"));
+            this.vista.txtCP.setText(this.rsEmp.getString("Codigo_postal"));
         }catch(Exception e){
             System.out.println("ModEmp: " + e);
         }
