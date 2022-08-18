9ad0017ed63b84ba54b8c323d7408ddaa6655d30
IngCarrillo <105088732+IngCarrillo@users.noreply.github.com>
2022-08-17 23:32:39 -0600

    ticketLoMejorQueSePudo
    oi

--- src/controlador/controlGenFactura.java
+++ src/controlador/controlGenFactura.java
@@ -62,52 +62,59 @@ public class controlGenFactura {
             encabezado.addCell(fecha);
             doc.add(encabezado);
 
-            Paragraph cli = new Paragraph();
-            cli.add(Chunk.NEWLINE);
-            cli.add("Datos del cliente\n\n");
-            doc.add(cli);
+//            Paragraph cli = new Paragraph();
+//            cli.add(Chunk.NEWLINE);
+//            cli.add("Datos del cliente\n\n");
+//            doc.add(cli);
+//
+//            PdfPTable tablacli = new PdfPTable(4);
+//            tablacli.setWidthPercentage(100);
+//            tablacli.getDefaultCell().setBorder(0);
+//            float[] colCli = new float[]{20f, 50f, 30f, 40f};
+//            tablacli.setWidths(colCli);
+//            tablacli.setHorizontalAlignment(Element.ALIGN_LEFT);
+//            PdfPCell cl1 = new PdfPCell(new Phrase("Nombre",negrita));
+//            cl1.setBorder(0);
+//            tablacli.addCell(cl1);
+//            String algo=nomC;
+//            tablacli.addCell(algo);
+//            doc.add(tablacli);
 
-            PdfPTable tablacli = new PdfPTable(4);
-            tablacli.setWidthPercentage(100);
-            tablacli.getDefaultCell().setBorder(0);
-            float[] colCli = new float[]{20f, 50f, 30f, 40f};
-            tablacli.setWidths(colCli);
-            tablacli.setHorizontalAlignment(Element.ALIGN_LEFT);
-            PdfPCell cl1 = new PdfPCell(new Phrase("Nombre",negrita));
-            cl1.setBorder(0);
-            tablacli.addCell(cl1);
-            String algo=nomC;
-            tablacli.addCell(algo);
-            doc.add(tablacli);
+            double total = 0.0;
 
             PdfPTable tablaProdPDF = new PdfPTable(4);
+            PdfPCell cell = new PdfPCell();
             tablaProdPDF.setWidthPercentage(100);
             tablaProdPDF.getDefaultCell().setBorder(0);
             float[] colProd = new float[]{20f, 50f, 30f, 40f};
-            tablaProdPDF.setWidths(colCli);
+            tablaProdPDF.setWidths(colProd);
             tablaProdPDF.setHorizontalAlignment(Element.ALIGN_LEFT);
-            PdfPCell pro1 = new PdfPCell(new Phrase("ID",negrita));
             PdfPCell pro2 = new PdfPCell(new Phrase("Nombre",negrita));
             PdfPCell pro3 = new PdfPCell(new Phrase("Precio",negrita));
             PdfPCell pro4 = new PdfPCell(new Phrase("Cantidad",negrita));
-            pro1.setBorder(0);
+            PdfPCell pro5 = new PdfPCell(new Phrase("Total",negrita));
             pro2.setBorder(0);
             pro3.setBorder(0);
             pro4.setBorder(0);
+            pro5.setBorder(0);
+            tablaProdPDF.addCell(pro2);
+            tablaProdPDF.addCell(pro3);
+            tablaProdPDF.addCell(pro4);
+            tablaProdPDF.addCell(pro5);
             for(int i=0;i<tablaProd.getRowCount();i++){
-                String Id=tablaProd.getValueAt(i, 0).toString();
                 String nombre=tablaProd.getValueAt(i, 1).toString();
                 String Precio=tablaProd.getValueAt(i, 2).toString();
                 String Cant=tablaProd.getValueAt(i, 3).toString();
-                tablaProdPDF.addCell(Id);
+                total = total + (Double.parseDouble(tablaProd.getValueAt(i, 3).toString())*(Double.parseDouble(tablaProd.getValueAt(i, 2).toString())));
                 tablaProdPDF.addCell(nombre);
                 tablaProdPDF.addCell(Precio);
                 tablaProdPDF.addCell(Cant);
+                tablaProdPDF.addCell(String.valueOf(total));
+                System.out.println(total);
             }
-            tablaProdPDF.addCell(pro1);
-            tablaProdPDF.addCell(nomC);
+
             doc.add(tablaProdPDF);
-            
+
             doc.close();
             archivo.close();
         }catch(Exception e){
